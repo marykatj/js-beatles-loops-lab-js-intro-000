9151c59afa692c331e9572ed4344ee342b68b471
Mary-Kate James <marykatj@gmail.com>
2018-01-12 19:42:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(arrayMusician, arrayInstrument) {
+  var newArray = array[];
+  for (let i = 0; i < 2) {
+  newArray.push(`${arrayMusician} plays ${arrayInstrument}`);
+  }
+  return newArray;
+}
