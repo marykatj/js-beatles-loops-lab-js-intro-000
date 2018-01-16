182156fa0d09e0ef3ca6cdd7904f34479cfc76dd
Mary-Kate James <marykatj@gmail.com>
2018-01-16 18:02:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function theBeatlesPlay(musicians, instruments) {
   var empty = [];
   var newArray = [];
   for (let i = 0; i < musicians.length; ++i) {
-      newArray.push(`${musicians} plays ${instruments}.`);
+      newArray.push(`${musicians[i]} plays ${instruments[i]}.`);
     }
   return newArray;
 }
