dce373618b892358c5a9b0b31e052bf9d1fe1bb2
Mary-Kate James <marykatj@gmail.com>
2018-01-16 18:07:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-function theBeatlesPlay(musician, instrument) {
+const musicians = [];
+const instruments = [];
+
+function theBeatlesPlay(musicians, instruments) {
   var empty = [];
-  for (let i = 0; i < 4; i) {
-     return empty.push(`${musician} plays ${instrument}.`);
+  var newArray = [];
+  for (let i = 0; i < musicians.length; ++i) {
+      newArray.push(`${musicians[i]} plays ${instruments[i]}`);
     }
+  return newArray;
 }
 
 const facts = [];
 function johnLennonFacts(facts) {
-  while (facts > 0)
-  console.log("!!!");
+  let i = 0
+  var newArray = [];
+  while (i < facts.length) {
+    newArray[i] = facts[i] +"!!!"
+    ++i;
+  }
+  return newArray;
 }
+
+function iLoveTheBeatles(number) {
+  var empty = [];
+  var newArray = [];
+  do {
+    newArray.push("I love the Beatles!");
+    number++;
+  } while (number < 15);
+return newArray;
+}
+
+/* function theBeatlesPlay(musician, instrument) {
+  var empty = [];
+  var newArray = [];
+  for (let i = 0; i < 4; i++) {
+      newArray = empty.push(`${musician} plays ${instrument}.`);
+    }
+    return newArray;
+} */
